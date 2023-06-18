16c6b11335084afa571c7562a04c3d87210e8fe1
ibotim <60549443+ibotim@users.noreply.github.com>
2023-06-18 22:01:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ apt-get update
 apt-get install software-properties-common
 apt-add-repository  ppa:ansible/ansible
 apt-get update
-apt-get install ansible
+apt-get install ansible -y
 ansible-pull -U https://github.com/ibotim/ansible.git
